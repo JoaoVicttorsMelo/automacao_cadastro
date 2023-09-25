fd128fb3702acce1adc66dc23028e79c619ac1c7
joaovicttorsmelo <joaovicttorsilveiramelo@gmail.com>
2023-09-25 20:31:28 -0300

    Realizando o login e senha de forma automatica pelo sistema

--- codigo.py
+++ codigo.py
@@ -1,9 +1,28 @@
 import pyautogui as gui
+import time
 
 
-gui.PAUSE = 0.5
-
+gui.PAUSE = 1
 gui.press('win')
 gui.write('opera')
 gui.press('enter')
+time.sleep(2)
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+gui.write(link)
+gui.press('enter')
+time.sleep(6)
+gui.click(x=447, y=390)
+
+gui.write('teste@gmail.com')
+time.sleep(4)
+gui.press('tab')
+gui.write('1234')
+gui.press('tab')
+gui.press('enter')
+time.sleep(4)
+gui.press('tab')
+
+
+#print(gui.position())
+
 
